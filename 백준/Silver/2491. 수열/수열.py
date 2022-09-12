89419bff2e35d7cd22695ad76851021471a2ae58
cmkds <107685502+cmkds@users.noreply.github.com>
2022-09-13 08:34:40 +0900

    [Silver IV] Title: 수열, Time: 124 ms, Memory: 32404 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.py
@@ -0,0 +1,35 @@
+num = int(input())
+
+lst = list(map(int,input().split()))
+
+upCnt = 1
+downCnt = 1
+maxCnt = 1
+for i in range(num-1):
+    if lst[i] == lst[i+1]:
+        upCnt +=1
+        downCnt +=1
+        if i == (num-2):
+            if upCnt > maxCnt:
+                maxCnt = upCnt
+            if downCnt > maxCnt:
+                maxCnt = downCnt
+
+    elif lst[i] <= lst[i+1]:
+        upCnt +=1
+        if downCnt > maxCnt:
+            maxCnt = downCnt
+        downCnt = 1
+        if i == (num - 2):
+            if upCnt > maxCnt:
+                maxCnt = upCnt
+    else:
+        downCnt +=1
+        if upCnt > maxCnt:
+            maxCnt = upCnt
+        upCnt = 1
+        if i == (num - 2):
+            if downCnt > maxCnt:
+                maxCnt = downCnt
+
+print(maxCnt)
