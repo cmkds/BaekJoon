fc09feb0ee21e5757fe6ca91de32f62a66ffe947
cmkds <107685502+cmkds@users.noreply.github.com>
2022-09-13 17:37:09 +0900

    [Bronze II] Title: 나머지, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,12 +1,6 @@
-lst = [int(input()) for _ in range(10)]
+se = set()
 
-chkLst = [0]*42
+for _ in range(10):
+    se.add(int(input()) % 42)
 
-for i in lst:
-    chkLst[i%42] += 1
-
-cnt = 0
-for i in chkLst:
-    if i:
-        cnt +=1
-print(cnt)
+print(len(se))
