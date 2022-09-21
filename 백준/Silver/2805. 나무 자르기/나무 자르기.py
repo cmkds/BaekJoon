1ef9d707ad825f48c364061d69e901756ddc8877
cmkds <107685502+cmkds@users.noreply.github.com>
2022-09-21 13:00:25 +0900

    [Silver II] Title: 나무 자르기, Time: 2524 ms, Memory: 262808 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,20 @@
+n , m = map(int,input().split())
+
+lst = list(map(int,input().split()))
+
+lst.sort()
+k = lst[-1]
+
+cnt = 0
+dumi = 0
+while cnt < m:
+    k -=1
+    for i in reversed(lst):
+        if i > k:
+            lst.pop()
+            dumi +=1
+        else:
+            break
+    cnt += dumi
+
+print(k)
